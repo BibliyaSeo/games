a864e149ce579a58086992ea872275a908017ca8
BibliyaSeo <seongkyeong.seo@gmail.com>
2022-10-21 17:08:05 +0900

    Finish: Game_Candy_Crush

--- game_candy_crush/src/App.tsx
+++ game_candy_crush/src/App.tsx
@@ -1,5 +1,47 @@
+import { useEffect } from "react";
+import Board from "./components/Board";
+import { moveBelow, updateBoard } from "./store";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { createBoard } from "./utils/createBoard";
+import {
+  formulaForColumnOfFour,
+  formulaForColumnOfThree,
+  generateInvalidMoves,
+} from "./utils/formulas";
+import {
+  checkForColumnOfFour,
+  checkForColumnOfThree,
+  checkForRowOfFour,
+  checkForRowOfThree,
+} from "./utils/moveCheckLogic";
+
 function App() {
-  return <div>App</div>;
+  const dispatch = useAppDispatch();
+  const board = useAppSelector(({ candyCrush: { board } }) => board);
+  const boardSize = useAppSelector(({ candyCrush: { boardSize } }) => boardSize);
+
+  useEffect(() => {
+    dispatch(updateBoard(createBoard(boardSize)));
+  }, [boardSize, dispatch]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const newBoard = [...board];
+      checkForColumnOfFour(newBoard, boardSize, formulaForColumnOfFour(boardSize));
+      checkForRowOfFour(newBoard, boardSize, generateInvalidMoves(boardSize, true));
+      checkForColumnOfThree(newBoard, boardSize, formulaForColumnOfThree(boardSize));
+      checkForRowOfThree(newBoard, boardSize, generateInvalidMoves(boardSize));
+      dispatch(updateBoard(newBoard));
+      dispatch(moveBelow());
+    }, 150);
+    return () => clearInterval(timeout);
+  }, [board, boardSize, dispatch]);
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Board />
+    </div>
+  );
 }
 
 export default App;
---
+++ game_candy_crush/src/utils/moveCheckLogic.ts
@@ -0,0 +1,72 @@
+export const checkForColumnOfFour = (
+  newBoard: string[],
+  boardSize: number,
+  formulaForColumnOfFour: number
+) => {
+  for (let i: number = 0; i <= formulaForColumnOfFour; i++) {
+    const columnOfFour: number[] = [i, i + boardSize, i + boardSize * 2, i + boardSize * 3];
+    const decidedColor: string = newBoard[i];
+    const isBlank: boolean = newBoard[i] === "";
+
+    if (columnOfFour.every((candy: number) => newBoard[candy] === decidedColor && !isBlank)) {
+      columnOfFour.forEach((candy: number) => (newBoard[candy] = ""));
+      return true;
+    }
+  }
+};
+
+export const checkForRowOfFour = (
+  newBoard: String[],
+  boardSize: number,
+  invalidMovesForColumnOfFour: number[]
+) => {
+  for (let i: number = 0; i < boardSize * boardSize; i++) {
+    const rowOfFour = [i, i + 1, i + 2, i + 3];
+    const decidedColor = newBoard[i];
+
+    const isBlank = newBoard[i] === "";
+
+    if (invalidMovesForColumnOfFour.includes(i)) continue;
+    if (rowOfFour.every((candy) => newBoard[candy] === decidedColor && !isBlank)) {
+      rowOfFour.forEach((candy) => (newBoard[candy] = ""));
+      return true;
+    }
+  }
+};
+
+export const checkForColumnOfThree = (
+  newBoard: String[],
+  boardSize: number,
+  formulaForColumnOfThree: number
+) => {
+  for (let i = 0; i <= formulaForColumnOfThree; i++) {
+    const columnOfThree = [i, i + boardSize, i + boardSize * 2];
+    const decidedColor = newBoard[i];
+    const isBlank = newBoard[i] === "";
+
+    if (columnOfThree.every((candy) => newBoard[candy] === decidedColor && !isBlank)) {
+      columnOfThree.forEach((candy) => (newBoard[candy] = ""));
+      return true;
+    }
+  }
+};
+
+export const checkForRowOfThree = (
+  newBoard: string[],
+  boardSize: number,
+  invalidMovesForColumnOfThree: number[]
+) => {
+  for (let i = 0; i < boardSize * boardSize; i++) {
+    const rowOfThree = [i, i + 1, i + 2];
+    const decidedColor = newBoard[i];
+
+    const isBlank = newBoard[i] === "";
+
+    if (invalidMovesForColumnOfThree.includes(i)) continue;
+
+    if (rowOfThree.every((candy) => newBoard[candy] === decidedColor && !isBlank)) {
+      rowOfThree.forEach((candy) => (newBoard[candy] = ""));
+      return true;
+    }
+  }
+};
